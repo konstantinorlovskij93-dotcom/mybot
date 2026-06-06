09ba4b089a86f583d947f20b67f8672da37b74a3
konstantinorlovskij93-dotcom <konstantinorlovskij93@gmail.com>
2026-06-06 19:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+import os
+from aiogram import Bot, Dispatcher
+# Этот кусочек кода просто откроет нужный для Render порт
+if __name__ == '__main__':
+    import asyncio
+    from aiohttp import web
+    async def handle(request):
+        return web.Response(text="Bot is alive")
+    app = web.Application()
+    app.router.add_get('/', handle)
+    # Запуск веб-сервера параллельно с ботом
+    loop = asyncio.get_event_loop()
+    loop.create_task(dp.start_polling(bot))
+    web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
